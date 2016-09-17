dcdda3d2e6257a5ebf6869f04632658393a9332a
Aditya <ladwa.aditya@gmail.com>
2016-09-17 15:30:29 +0530

    Added Cloudant dependency and connected to databse successfully

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ var express = require('express');
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
+var Cloudant = require('cloudant');
+
+// var cloudant = Cloudant("https://86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix.cloudant.com");
+
+
+// Initialize the library with my account.
+var cloudant = Cloudant("https://86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix:8f2daf28e6ae397ca8ac843a2fe4f325f893bda96e366c5b954ce494d6f6d3d2@86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix.cloudant.com");
+
+cloudant.db.list(function(err, allDbs) {
+  console.log('All my databases: %s', allDbs.join(', '))
+});
+
 
 // create a new express server
 var app = express();
