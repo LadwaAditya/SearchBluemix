5af8fc8addd9be850e00830092598afacf1e5485
Aditya <ladwa.aditya@gmail.com>
2016-09-19 11:57:43 +0530

    Added service binding and updated app.js

--- app.js
+++ app.js
@@ -14,13 +14,7 @@ var cfenv = require('cfenv');
 var Cloudant = require('cloudant');
 
 // Initialize the library with my account.
-var cloudant = Cloudant("https://86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix:8f2daf28e6ae397ca8ac843a2fe4f325f893bda96e366c5b954ce494d6f6d3d2@86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix.cloudant.com");
 
-cloudant.db.list(function(err, allDbs) {
-  console.log('All my databases: %s', allDbs.join(', '))
-});
-
-var db = cloudant.db.use('users');
 
 // create a new express server
 var app = express();
@@ -29,11 +23,7 @@ app.use(bodyParser.json());
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/search/:name',function(req,res){
-  db.search('userDoc', 'userSearch', {q:'name:' + req.params.name }, function(er, result) {
-    res.send(result);
-  });
-});
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
@@ -42,4 +32,12 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+app.get('/api/search/:name',function(req,res){
+  var cloudant = Cloudant("https://86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix:8f2daf28e6ae397ca8ac843a2fe4f325f893bda96e366c5b954ce494d6f6d3d2@86d20f68-ae6d-47e5-8adc-f9c222b6af94-bluemix.cloudant.com");
+  var db = cloudant.db.use('users');
+  db.search('userDoc', 'userSearch', {q:'name:' + req.params.name }, function(er, result) {
+    res.json(result);
+  });
 });
